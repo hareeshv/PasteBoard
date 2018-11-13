caaa6d84f3b6649fc95e2a274ee4745cb92a2da2
hareeshv <hareesh.veligeti@gmail.com>
2018-11-13 12:01:14 +0530

    Created spring boot mvn-java-2.1.0 project.

---
+++ src/main/java/com/prj/pasteboard/PasteboardApplication.java
@@ -0,0 +1,12 @@
+package com.prj.pasteboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasteboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasteboardApplication.class, args);
+	}
+}
